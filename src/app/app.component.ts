cbf805e4c76da248d46cded5f6c1b482f68b8573
Andrey <rojer9999@gmail.com>
2017-11-28 17:23:54 +0200

    array iteration implement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    users = [{ name: 'WFM 1' }, { name: 'WFM 2' }, { name: 'WFM 3' }, { name: 'WFM 4' }, { name: 'WFM 5' }];
 }
