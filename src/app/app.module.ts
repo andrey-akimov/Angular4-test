4651b24079851946bbea9d7181170f96a2d2d50e
Andrey <rojer9999@gmail.com>
2017-11-28 21:31:35 +0200

    filter function added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HttpModule } from '@angular/http';
 import { HoverDirective } from './hover.directive';
+import { SearchPipe } from './search.pipe';
 
 @NgModule({
-    declarations: [AppComponent, UserComponent, HoverDirective],
-    imports: [BrowserModule, HttpModule],
+    declarations: [AppComponent, UserComponent, HoverDirective, SearchPipe],
+    imports: [BrowserModule, HttpModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent]
 })
